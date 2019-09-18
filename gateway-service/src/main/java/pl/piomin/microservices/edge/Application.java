5ada14a88e92d90e5add529aee760a2d58da1723
root <root@ip-172-31-25-46.ec2.internal>
2019-09-18 13:41:52 +0000

    commit code

--- gateway-service/src/main/java/pl/piomin/microservices/edge/Application.java
+++ gateway-service/src/main/java/pl/piomin/microservices/edge/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableZuulProxy
 @RestController
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
