627769c70fdba0c8fea858315ddc088352ffb85e
root <root@ip-172-31-25-46.ec2.internal>
2019-09-26 11:59:39 +0000

    discovery address change

--- discovery-service/src/main/java/pl/piomin/microservices/eureka/Application.java
+++ discovery-service/src/main/java/pl/piomin/microservices/eureka/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer
 public class Application {
@@ -11,5 +12,6 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
+	
 }
