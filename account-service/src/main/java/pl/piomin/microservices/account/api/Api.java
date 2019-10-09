5aa2a02104dc65f421291c198d833e6c365c1bf1
root <root@ip-172-31-25-46.ec2.internal>
2019-10-09 12:20:43 +0000

    jenkins File

--- account-service/src/main/java/pl/piomin/microservices/account/api/Api.java
+++ account-service/src/main/java/pl/piomin/microservices/account/api/Api.java
@@ -27,6 +27,7 @@ public class Api {
 		accounts.add(new Account(5, 1, "555555"));
 		accounts.add(new Account(6, 2, "666666"));
 		accounts.add(new Account(7, 2, "777777"));
+		accounts.add(new Account(8, 2, "888888"));
 	}
 	
 	@RequestMapping("/accounts/{number}")
